840a7aa07f25e93907956f8203d4c267e413113d
rmsoft1 <rmsoftware1@gmail.com>
2023-11-24 08:14:41 -0500

    Clean up usings

--- RMSoftware.Http/QuickServer.cs
+++ RMSoftware.Http/QuickServer.cs
@@ -1,12 +1,7 @@
 ﻿using HttpMultipartParser;
-using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 
 namespace RMSoftware.Http
